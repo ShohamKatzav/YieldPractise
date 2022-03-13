c0e7daf36efc8c113ea5cc3955584436fc286039
Shoham Katzav <shohamkatzav95@gmail.com>
2022-03-13 20:22:28 +0200

    Add project files.

---
+++ YieldPractise/Agency.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldPractise
+{
+    internal class Agency
+    {
+        public Car[] Cars;
+        public IEnumerator GetEnumerator()
+        {
+            foreach (Car car in Cars)
+                yield return car;
+        }
+        public IEnumerable GetCars(string maker)
+        {
+            foreach(Car car in Cars)
+                if (car.Maker == maker) yield return car;
+        }
+        public IEnumerable GetCars(int model)
+        {
+            foreach (Car car in Cars)
+                if (car.ModelYear == model) yield return car;
+        }
+    }
+}
---
+++ YieldPractise/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldPractise
+{
+    internal class Car
+    {
+        public string? Maker;
+        public int ModelYear;
+        public override string ToString() => $"{ModelYear}, {Maker}";
+    }
+}
